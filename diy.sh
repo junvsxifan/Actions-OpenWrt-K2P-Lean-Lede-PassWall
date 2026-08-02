dffd5aa2cde2bae78cd03276a4414f701fe51b5d
junvsxifan <junvsxifan666@gmail.com>
2026-08-02 16:45:43 +0800

    Delete diy.sh

--- diy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# 拉取Passwall插件，存在则跳过，避免克隆报错
-[ ! -d package/passwall ] && git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-[ ! -d package/passwall-packages ] && git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
